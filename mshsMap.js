1a4d16d5a8177978fc7b3fb97f4bb0bf6d666cbf
njc1583 <njc1583@gmail.com>
2017-09-08 10:39:23 -0500

    Update mshsMap.js

--- mshsMap.js
+++ mshsMap.js
@@ -4,6 +4,46 @@ var MAP_HEIGHT = 645;
 var mapContainer = document.getElementById("map");
 var map = new Raphael(mapContainer, MAP_WIDTH, MAP_HEIGHT);
 
+var mathRoomStyle = {
+  fill: "#d81515",
+  stroke: "#660f0f",
+  "stroke-wdith": 1,
+  "stroke-linejoin": "miter",
+  cursor: "pointer"
+};
+
+var engRoomStyle = {
+  fill: "c40b0b",
+  stroke: "#6d0d0d",
+  "stroke-wdith": 1,
+  "stroke-linejoin": "miter",
+  cursor: "pointer"
+};
+
+var sciRoomStyle = {
+  fill: "#1b9b17",
+  stroke: "#093d07",
+  "stroke-wdith": 1,
+  "stroke-linejoin": "miter",
+  cursor: "pointer"
+};
+
+var histRoomStyle = {
+  fill: "#dd800d",
+  stroke: "#96570a",
+  "stroke-wdith": 1,
+  "stroke-linejoin": "miter",
+  cursor: "pointer"
+};
+
+var langRoomStyle = {
+  fill: "#f7ec20",
+  stroke: "#a59f2b",
+  "stroke-wdith": 1,
+  "stroke-linejoin": "miter",
+  cursor: "pointer"
+};
+
 var miscRoomStyle = {
   fill: "#ff66ff",
   stroke: "#660066",
@@ -17,13 +57,36 @@ var mathRooms = {};
 var engRooms = {};
 var sciRooms = {};
 var histRooms = {};
+var langRooms = {};
 var miscRooms = {};
 
-var regions = {mathRooms, engRooms, sciRooms, histRooms, miscRooms};
+//Not sure this is necessary
+var regions = {mathRooms, engRooms, sciRooms, histRooms, langRooms, miscRooms};
 
+//Create all miscRooms and assign map locations
 miscRooms["auditorium"] = map.rect("m 100, 600, 20, 20 z");
 
 //Set each rooms to different styles
+for(var roomName in mathRooms){
+  mathRooms[roomName].attr(mathRoomStyle);  
+}
+
+for(var roomName in engRooms){
+  engRooms[roomName].attr(engRoomStyle);
+}
+
+for(var roomName in sciRooms){
+  sciRooms[roomName].attr(sciRoomStyle);
+}
+
+for(var roomName in histRooms){
+  histRooms[roomName].attr(histRoomStyle);
+}
+
+for(var roomName in langRooms){
+  langRooms[roomName].attr(langRoomStyle);
+}
+
 for(var roomName in miscRooms){
-  regions[roomName].attr(miscRoomStyle);
+  miscRooms[roonName].attr(miscRoomStyle);
 }
